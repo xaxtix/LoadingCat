bb367758fd0ac45c7a13a689ee4c237a2db38bd3
Илья Самородов <is@poloniumarts.com>
2017-05-18 01:55:15 +0300

    initial

---
+++ app/src/main/java/com/example/com/loadingcat/LoadingCat.java
@@ -0,0 +1,186 @@
+package com.example.com.loadingcat;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.CornerPathEffect;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.graphics.PointF;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class LoadingCat extends View {
+
+    Rect drawingRect;
+    RectF drawingRectF;
+    RectF internalRectF;
+    RectF internalLeftRectF;
+    RectF internalRightRectF;
+
+    Paint strokePaint;
+
+    Matrix rotateMatrix;
+
+    float alph = 100;
+    boolean inc;
+
+    public LoadingCat(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+
+    public LoadingCat(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public LoadingCat(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        drawingRect = new Rect();
+        drawingRectF = new RectF();
+        internalRectF = new RectF();
+        internalLeftRectF = new RectF();
+        internalRightRectF = new RectF();
+
+        strokePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        strokePaint.setColor(ContextCompat.getColor(context, R.color.strokeColor));
+        strokePaint.setStyle(Paint.Style.STROKE);
+        strokePaint.setStrokeWidth(16);
+        strokePaint.setStrokeCap(Paint.Cap.ROUND);
+        strokePaint.setStrokeJoin(Paint.Join.ROUND);
+
+        rotateMatrix = new Matrix();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int sizeHalf = Math.min(getMeasuredHeight(), getMeasuredWidth()) >> 1;
+        int sizeQuad = (int) (sizeHalf * 0.4f);
+        int cX = getMeasuredWidth() >> 1;
+        int cY = getMeasuredHeight() >> 1;
+        int padding = (int) (strokePaint.getStrokeWidth() * 0.5f);
+
+        drawingRect.set(
+                cX - sizeHalf + padding,
+                cY - sizeHalf + padding,
+                cX + sizeHalf - padding,
+                cY + sizeHalf - padding
+        );
+
+        drawingRectF.set(drawingRect);
+        internalRectF.set(
+                drawingRect.left + sizeQuad,
+                drawingRect.top + sizeQuad,
+                drawingRect.right - sizeQuad,
+                drawingRect.bottom - sizeQuad
+        );
+
+        int earSize = ((int) (drawingRectF.bottom - internalRectF.bottom)) / 4;
+
+        internalLeftRectF.set(
+                internalRectF.left - earSize,
+                internalRectF.top - earSize,
+                internalRectF.right + earSize,
+                internalRectF.bottom + earSize
+        );
+
+        internalRightRectF.set(
+                drawingRectF.left + earSize,
+                drawingRectF.top + earSize,
+                drawingRectF.right - earSize,
+                drawingRectF.bottom - earSize
+        );
+    }
+
+    int i = 0;
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int pX = drawingRect.centerX();
+        int pY = drawingRect.centerY();
+        int sizeHalf = drawingRect.height() >> 1;
+        int internalSizeHalf = ((int) internalRectF.height()) >> 1;
+        int earSize = ((int) (drawingRectF.bottom - internalRectF.bottom)) / 4;
+
+
+        if (inc) {
+            alph += 3;
+            if (alph > 300)
+                inc = false;
+        } else {
+            alph -= 3;
+            if (alph < 60)
+                inc = true;
+        }
+
+
+        canvas.rotate(i-=5,pX,pY);
+
+        rotateMatrix.setTranslate(0, 40);
+        Point point = new Point(pX + sizeHalf - earSize, pY);
+
+        //head
+        Path path = new Path();
+        path.arcTo(drawingRectF, alph, -alph);
+        transformPoint(point, rotateMatrix);
+        path.lineTo(point.x, point.y);
+        point.set(pX + internalSizeHalf + earSize, pY);
+        transformPoint(point, rotateMatrix);
+        path.lineTo(point.x, point.y);
+        path.arcTo(internalRectF, 0, alph);
+
+        //legs
+        rotateMatrix.reset();
+        rotateMatrix.setRotate(alph, pX, pY);
+        point.set(pX + internalSizeHalf + earSize, pY);
+        transformPoint(point, rotateMatrix);
+        path.lineTo(point.x, point.y);
+
+
+        path.arcTo(internalLeftRectF, alph, -15);
+        point.set(pX + sizeHalf - earSize, pY);
+        rotateMatrix.setTranslate(
+                0,
+                -(float) (internalLeftRectF.height() * Math.sin(Math.toRadians(15 * 0.5d)))
+        );
+        transformPoint(point, rotateMatrix);
+        rotateMatrix.setRotate(alph, pX, pY);
+        transformPoint(point, rotateMatrix);
+        path.lineTo(point.x, point.y);
+
+        path.arcTo(internalRightRectF, alph - 15, 15);
+
+        path.close();
+
+        canvas.drawPath(path, strokePaint);
+
+        invalidate();
+        //canvas.drawCircle(pX, pY, sizeHalf, strokePaint);
+    }
+
+    public void transformPoint(Point point, Matrix matrix) {
+        float[] pts = new float[2];
+
+        pts[0] = point.x;
+        pts[1] = point.y;
+
+        matrix.mapPoints(pts);
+
+        point.set((int) pts[0], (int) pts[1]);
+    }
+
+
+}
